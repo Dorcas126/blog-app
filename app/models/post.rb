e6e65bce523985e1db49d42dfbafa2f6a473e7e3
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-27 18:24:21 +0200

    process data models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  has_many :likes, foreign_key: 'posts_id'
+  has_many :comments, foreign_key: 'posts_id'
+  belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
+
+  def update_posts_counter
+    author.increment(:posts_counter)
+  end
+
+  :title
+  :author_id
+  :likes_counter
+
+  def last_five_comments
+    comments.last(5)
+  end
 end
