cdced104d1612e482716e9ef3c216aa42b1f1914
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-28 12:31:00 +0200

    process data models for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_many :posts, dependent: :destroy, foreign_key: 'author_id'
+    has_many :comments, foreign_key: 'author_id'
+    has_many :likes, foreign_key: 'author_id'
+  
+     :name
+     :posts_counter
+  
+    def last_three_posts
+      posts.last(3)
+    end
 end
