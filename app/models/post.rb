2a07f7206009af4d83d976278e92d403fd5109fa
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-29 17:01:53 +0200

    Validate post file

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,16 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'posts_id'
   belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
 
+
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :text, presence: true, length: { in: 1..250 }
+  validates :author_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likesCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def update_posts_counter
     author.increment(:posts_counter)
   end
+
 
   def last_five_comments
     comments.last(5)
