17e0da5e6a4bcdcff8edd19ea6a94f1b4016096e
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-30 19:48:41 +0200

    Remove blank line

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'posts_id'
   belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
 
-
   validates :title, presence: true, length: { in: 1..250 }
   validates :text, presence: true, length: { in: 1..250 }
   validates :author_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
