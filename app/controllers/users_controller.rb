4a3908d25c73267aecb5b9412d5b57b5b2637ef9
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-10-03 17:29:14 +0200

    made changes requested by users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def index
     puts :users
   end
 
   def show
-    user_id = params[:id].to_i
-    puts User.find(user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'user#index'
+  root 'users#index'
   # get 'post/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
