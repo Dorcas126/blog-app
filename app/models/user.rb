2596a4d966b7b1d5a478b820b1dd58bc9e88bde1
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-10-10 16:47:37 +0200

    correct linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,  :confirmable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   # validates :name, presence: true, length: { in: 1..250 }
   # validates :photo, presence: true, length: { in: 1..250 }
   # validates :bio, presence: true, length: { in: 1..250 }
-  :email
-  # :encrypted_password
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
